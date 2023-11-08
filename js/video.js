cb8f52a96c0d53ad44c6b5eb558b8fc3a8567495
sat048 <123526479+sat048@users.noreply.github.com>
2023-11-08 09:13:39 -0500

    Add files via upload

--- js/video.js
+++ js/video.js
@@ -5,11 +5,10 @@ window.addEventListener("load", function() {
 
 });
 
-
-
 document.querySelector("#play").addEventListener("click", function() {
 	console.log("Play Video");
 	video.play();
+	document.querySelector("#volume").innerHTML = (video.volume * 100);
 });
 
 document.querySelector("#pause").addEventListener("click", function() {
@@ -17,5 +16,45 @@ document.querySelector("#pause").addEventListener("click", function() {
 	video.pause();
 });
 
+document.querySelector("#slower").addEventListener("click", function() {
+	currentVol = video.playbackRate;
+	const percChange = 0.1;
+	currentVol = currentVol - (currentVol * percChange);
+	video.playbackRate = currentVol;
+	console.log("New Speed: " + video.playbackRate);
+});
 
+document.querySelector("#faster").addEventListener("click", function() {
+	fastVol = video.playbackRate;
+	fastVol = (fastVol / 0.9);
+	video.playbackRate = fastVol;
+	console.log("New Speed: " + video.playbackRate);
+	//fix
+});
 
+document.querySelector("#skip").addEventListener("click", function() {
+	time = video.currentTime;
+	video.currentTime = time + 10;
+	console.log(video.currentTime);
+	if (video.currentTime >= video.duration) {video.currentTime = 0}	
+});
+
+document.querySelector("#mute").addEventListener("click", function() {
+	video.muted = !video.muted;
+	if (video.muted == true) {document.querySelector("#mute").innerHTML = "Unmute"};
+	if (video.muted == false) {document.querySelector("#mute").innerHTML = "Mute"};
+});
+
+document.querySelector("#slider").addEventListener("click", function() {
+	video.volume = this.value / 100;
+	console.log(video.volume);
+	document.querySelector("#volume").innerHTML = this.value + "%";
+});
+
+document.querySelector("#vintage").addEventListener("click", function() {
+	video.classList.add("oldSchool");
+});
+
+document.querySelector("#orig").addEventListener("click", function() {
+	video.classList.remove("oldSchool");
+});
